8dac385fe417b1325c7c5e722021e79e40cb6ef9
Dera <stephenie7533@gmail.com>
2021-10-27 01:53:15 +0100

    Updates

---
+++ precache-manifest.8434398d03eb7802213090d59c7d4879.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8b2d14352028c9456ccf9031eea4ed",
+    "url": "/cocktails/index.html"
+  },
+  {
+    "revision": "0968314e42699473ec9e",
+    "url": "/cocktails/static/css/main.db6e84ad.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/cocktails/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktails/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0968314e42699473ec9e",
+    "url": "/cocktails/static/js/main.1ba51391.chunk.js"
+  },
+  {
+    "revision": "e448c4ac95b4b7fbb491",
+    "url": "/cocktails/static/js/runtime-main.42b4d6f2.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/cocktails/static/media/logo.f3ad7664.svg"
+  }
+]);
