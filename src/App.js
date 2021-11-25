c9ac840e7a4e74222d9b0c679df0d8671f1680e5
Dera <stephenie7533@gmail.com>
2021-11-25 10:57:01 +0100

    commited for heroku

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+// import pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+// import components
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/about">
+          <About />
+        </Route>
+
+        <Route exact path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route exact path="*">
+        <Redirect exact from="/cocktail/:id" to="/" />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>This is a Cocktail web application made by Steven. This application consumes the cocktail Database API and returns specified information about each cocktail.</p>
+    </section>
+  )
+}
+export default About
